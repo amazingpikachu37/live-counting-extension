396caa812b90246f64d2bde554c90c4ba42e01cb
amazingpikachu37 <73712117+amazingpikachu37@users.noreply.github.com>
2021-03-08 00:40:19 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1077,9 +1077,9 @@ var ReplyTimes;
         }
 	console.log(validcount3);
 	if (Notification.permission === "granted") {
-	    //if (validcount3 === '000') {
+	    if (validcount3.substr(5) == '10') {
      	    	showNotification();
-	    //}
+	    }
    	} else if (Notification.permission !== "denied") {
       	    Notification.requestPermission().then(permission => {
         	console.log(permission);
